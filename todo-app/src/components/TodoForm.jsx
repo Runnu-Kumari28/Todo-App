f3c236c677e05c02cd2aba377c57ff875ea73734
runnu_kumari <runnukumari9901@gmail.com>
2024-09-11 18:25:40 +0530

    Add TodoList to show created Task

--- todo-app/src/components/TodoForm.jsx
+++ todo-app/src/components/TodoForm.jsx
@@ -1,32 +1,49 @@
-import React from 'react'; 
+import React, { useState } from 'react';
 import './style.css';
-function TodoForm() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        event.target.reset();
-    };
 
-    return (
-        <form onSubmit={handleSubmit} className='formElements'>
-            <label htmlFor='todo'>
-                <input 
-                    type='text'
-                    name='todo'
-                    id='todo'
-                    placeholder='Enter your Task'
-                />
-            </label>
-            <label htmlFor='desc'>
-                <input 
-                    type='text'
-                    name='desc'
-                    id='desc'
-                    placeholder='Task Description'
-                />
-            </label>
-            <button>Submit</button>
-        </form>
-    )
+function TodoForm({ addTask }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const newTask = { title, description };
+    // Pass the new task to the parent component
+    addTask(newTask);
+    
+    event.target.reset();
+    setTitle('');
+    setDescription('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='formElements'>
+      <label htmlFor='todo'>
+        <input 
+          type='text'
+          name='todo'
+          id='todo'
+          placeholder='Enter your Task'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
+
+      <label htmlFor='description'>
+        <input 
+          type='text'
+          name='description'
+          id='description'
+          placeholder='Task Description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default TodoForm;
